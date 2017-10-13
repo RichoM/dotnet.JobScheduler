730f4a478c60b08ae8bb6411ce4fd2c695b27d68
RichoM <richi.moran@gmail.com>
2017-10-13 11:46:38 -0300

    Implemented Promise.All(..)

--- JobScheduling/Promise.cs
+++ JobScheduling/Promise.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace JobScheduling
@@ -14,6 +16,51 @@ namespace JobScheduling
     public interface IPromise<T> : IPromise
     {
         IPromise<T> Then(Action<T> action);
+    }
+
+    public static class Promise
+    {
+        public static IPromise All(IEnumerable<IPromise> promises)
+        {
+            promises = promises.ToArray();
+            int total = promises.Count();
+            int count = 0;
+            var allPromise = new Promise<object>();
+            foreach (var promise in promises)
+            {
+                promise.Then(() =>
+                {
+                    Interlocked.Increment(ref count);
+                    if (Interlocked.CompareExchange(ref count, 0, 0) >= total)
+                    {
+                        allPromise.Resolve(null);
+                    }
+                });
+            }
+            return allPromise;
+        }
+
+        public static IPromise<IEnumerable<T>> All<T>(IEnumerable<IPromise<T>> promises)
+        {
+            promises = promises.ToArray();
+            int total = promises.Count();
+            int count = 0;            
+            ConcurrentQueue<T> results = new ConcurrentQueue<T>();
+            var allPromise = new Promise<IEnumerable<T>>();
+            foreach (var promise in promises)
+            {
+                promise.Then(result =>
+                {
+                    results.Enqueue(result);
+                    Interlocked.Increment(ref count);
+                    if (Interlocked.CompareExchange(ref count, 0, 0) >= total)
+                    {
+                        allPromise.Resolve(results);
+                    }
+                });
+            }
+            return allPromise;
+        }
     }
     
     public class Promise<T> : IPromise<T>, IPromise
@@ -61,7 +108,15 @@ namespace JobScheduling
             while (actions.Count > 0)
             {
                 Action<T> action = actions.Dequeue();
-                action(value);
+                try
+                {
+                    action(value);
+                }
+                catch (Exception ex)
+                {
+                    // TODO(Richo): Log Exception!
+                    Console.WriteLine(ex.ToString());
+                }
             }
         }
     }
